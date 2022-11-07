6d39496b7d5dd90c6e93fb0532e8cb103f2338af
Warren Dugan <warrendugan@gmail.com>
2022-11-07 14:07:21 -0800

    first commit

---
+++ apps/web/src/main.ts
@@ -0,0 +1,17 @@
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+} from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+import { appRoutes } from './app/app.routes';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+}).catch((err) => console.error(err));
